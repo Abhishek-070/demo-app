a764a488d1bd833d3819501c9764e919e9582b4d
Abhishek-070 <dabhishek175@gmail.com>
2021-06-02 01:56:27 +0530

    Initial commit

---
+++ src/main/java/com/example/demoapp/DemoAppApplication.java
@@ -0,0 +1,26 @@
+package com.example.demoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class DemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppApplication.class, args);
+	}
+	@PostConstruct
+    public void init()
+    {
+        Logger log = LoggerFactory.getLogger(App.class);
+        log.info("Java app started");
+    }
+
+    public String getStatus() {
+        return "OK";
+    }
+
+}
